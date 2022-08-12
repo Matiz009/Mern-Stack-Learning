a0279547fcbd9cf66fa98af8ce03c112e08e0252
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-12 17:30:38 +0500

    practising if else

---
+++ javascript/blackjack.js
@@ -0,0 +1,23 @@
+let firstCard = 10;
+let secondCard = 11;
+let sum = firstCard + secondCard + 4;
+let hasBlackJack = false;
+let isAlive = true;
+let message = " ";
+// 1. Declare a variable called message and assign its value to an empty string
+
+// 2. Reassign the message variable to the string we're logging out
+if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+    console.log(message);
+} else if (sum === 21) {
+    message = "Woo hoo! You've got Blackjack! 🥳";
+    console.log(message);
+    hasBlackJack = true;
+} else {
+    message = "You're out of the game! 😭";
+    console.log(message);
+    isAlive = false;
+}
+
+// 3. Log it out!
