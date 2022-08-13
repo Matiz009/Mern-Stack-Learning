f3ba3e71928074afe67f02868a6388a67fe380ba
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-13 13:01:40 +0500

    objects in js

--- javascript/app.js
+++ javascript/app.js
@@ -62,3 +62,29 @@ console.log(concatenatedMessage);
 
 let welcome = document.getElementById("welcome-el");
 welcomeEl.innerText = greeting + name;
+
+
+//Logical Operators
+let hasCompletedCourse = false;
+let givesCertificate = true;
+
+if (hasCompletedCourse === true && givesCertificate === true) {
+    generateCertificate();
+}
+
+function generateCertificate() {
+    console.log("Generating certificate....");
+}
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+
+// Create an if statement that checks that both variables are false.
+// If so, run the showSolution() function
+
+function showSolution() {
+    console.log("Showing the solution....");
+}
+
+if (hasSolutionChallenge === false && hasHintsLeft === false) {
+    showSolution();
+}
---
+++ javascript/objects.js
@@ -0,0 +1,28 @@
+let into = {
+    //objects in js
+    name: "John",
+    greeting: "Hello!",
+    age: 22,
+    hobby: "basketball",
+    location: "New York",
+    occupation: "Web Developer",
+    pet: "dog",
+};
+
+console.log(into.name);
+console.log(into.greeting);
+console.log(into.age);
+
+let castle = {
+    name: "Castle",
+    hasPark: true,
+    hasPool: true,
+    rooms: 10,
+    residents: ["John", "Jane", "Sam"],
+};
+
+console.log(castle.name);
+console.log(castle.hasPark);
+console.log(castle.hasPool);
+console.log(castle.rooms);
+console.log(castle.residents[2]);
