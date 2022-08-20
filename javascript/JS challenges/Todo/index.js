444f906fbf84448f7292175b9c1cfc0b6e6187d8
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-20 07:59:14 +0500

    delete button added

--- javascript/JS challenges/Todo/index.js
+++ javascript/JS challenges/Todo/index.js
@@ -1,6 +1,7 @@
 let input = document.getElementById("newToDo");
 let newTodo = document.getElementById("todos");
 let add_btn = document.getElementById("btnAdd");
+let delete_btn = document.getElementById("btn_del");
 add_btn.addEventListener("click", function() {
     if (input.value == "") {
         alert("Please enter something");
@@ -10,4 +11,7 @@ add_btn.addEventListener("click", function() {
         newTodo.appendChild(element);
         input.value = "";
     }
+});
+delete_btn.addEventListener("click", function() {
+    newTodo.innerHTML = "";
 });
