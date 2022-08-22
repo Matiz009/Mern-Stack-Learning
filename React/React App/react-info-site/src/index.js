c3cb0a88dc4134a1f63fc75af7e7f4cfc4e8145e
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-22 11:33:17 +0500

    styling added;

--- React/React App/react-info-site/src/index.js
+++ React/React App/react-info-site/src/index.js
@@ -1,27 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+
 
 const page = (
   <div>
-    <img
-      src="https://th.bing.com/th/id/OIP.JU4qreGiDoB2bzfyCfJYRQHaHa?pid=ImgDet&w=500&h=500&rs=1"
-      alt="logo"
-    />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on GitHub</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
+    <header>
+      <nav className="nav">
+        <img
+          src="https://th.bing.com/th/id/OIP.JU4qreGiDoB2bzfyCfJYRQHaHa?pid=ImgDet&w=500&h=500&rs=1"
+          alt="logo"
+          className="img"
+        />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+
+    <h1> Fun facts about React </h1>
+    <ol>
+      <li> Was first released in 2013 </li>
+      <li> Was originally created by Jordan Walke </li>
+      <li> Has well over 100 K stars on GitHub </li>
+      <li> Is maintained by Facebook </li>
+      <li> Powers thousands of enterprise apps, including mobile apps </li>
+    </ol>
+    <footer>
+      <small> ©2021 Matiz development.All rights reserved. </small>
+    </footer>
   </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(page);
-
-reportWebVitals();
