95ef49f66fd93102eb7ee66edf2a5b915317e3c9
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-25 22:04:25 +0500

    cards added

---
+++ React/React App/AirBnb clone/airbnb/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Katie from "../images/katie-zaferes.png";
+import star from "../images/star.png";
+export default function Card() {
+  return (
+    <div className="card">
+      <img src={Katie} className="card--image" alt="katie" />
+      <div className="card--stats">
+        <img src={star} alt="star" className="card--star" />
+        <span>5.0</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life Lessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From $136 </span>/ person
+      </p>
+    </div>
+  );
+}
