05ea67070e4ebe53e4fa7121627536562d75187b
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-26 22:16:50 +0500

    props added

--- React/React App/AirBnb clone/airbnb/src/App.js
+++ React/React App/AirBnb clone/airbnb/src/App.js
@@ -2,12 +2,32 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import pic from "./images/katie-zaferes.png";
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={pic}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
+
+/*
+Challenge: Pass props to the Card component and display that data
+
+- img ("katie-zaferes.png")
+- rating ("5.0")
+- reviewCount (6)
+- country (Whatever you want)
+- title ("Life Lessons with Katie Zaferes")
+- price (136)
+
+*/
